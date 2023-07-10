58ab302235c80370b568ae642b5b024614625c5e
anggunputrilestari12 <113159481+anggunputrilestari12@users.noreply.github.com>
2023-07-10 12:14:21 +0700

    Membuat file about.php pada folder Views/novel

---
+++ app/Views/novel/about.php
@@ -0,0 +1,38 @@
+<?= $this->extend('layout/layout') ?>
+
+<?= $this->section('content') ?>
+<div class="row">
+        <div class="col-md-12">
+            <table >
+                <tr>
+                    <th></th>
+                    <th>
+                        <h1>
+                            Tentang kami
+                        </h1>
+                    </th>
+                    <th></th>
+                </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <p>
+                            Selamat datang di Baca Novel, web terbaik untuk menikmati berbagai jenis Novel yang menarik.
+                            </p>
+                            <p>
+                            Di Baca Novel, kami berkomitmen untuk memberikan pengalaman mmengakses yang luar biasa bagi para pengguna kami. Kami menyajikan tampilan yang user-friendly, navigasi yang intuitif dan fitur-fitur canggih yang memudahkan Anda dalam menemukan Novel-novel favorit serta dapatkan akses ke katalog Novel yang lengkap dengan genre yang beragam mula dari fiksi, non fiksi, drama, komedi ,horor dan romansa.
+                            </p>
+                            <p>
+                            Selain itu kami juga memberikan informasi terkini mengenai detail Novel. Kami juga memahami pentingnya keamanan dan kenyamanan saat menikmati konten digital. Oleh karena itu, kami menjaga privasi pengguna dengan cermat dan mengutamakan kebijakan hak cipta serta legalitas dalam menyediakan novel-novel yang kami tampilkan
+                            </p>
+                            <p>
+                        </td>
+                        <td>
+                        </td>
+                    </tr>
+ 
+
+            </table>
+        </div>
+    </div>
+    <?= $this->endSection() ?>
