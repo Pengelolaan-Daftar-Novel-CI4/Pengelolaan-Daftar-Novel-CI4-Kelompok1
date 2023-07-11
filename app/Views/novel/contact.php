035bc67b2386a139b2ddfb9603340245fd933088
dianafnioktavia23 <113124849+dianafnioktavia23@users.noreply.github.com>
2023-07-11 17:51:23 +0700

    update contact

--- app/Views/novel/contact.php
+++ app/Views/novel/contact.php
@@ -3,8 +3,16 @@
 <?= $this->section('content'); ?>
 <div class="con">
     <div class="row">
-        <div class="col">
-            <h2>Contact us</h2>
+    <div class="row">
+    <div class="col-md-12">
+        <div class="card">
+        <div class="card-header">
+            <div class="row">
+             <div class="text-center">
+                <h2> Contact Us</h2>
+                </div>
+            </div>
+        </div>
 
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3972.383101489142!2d105.23031467427117!3d-5.358374353633897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e40c54fa0db33eb%3A0xe69b7788ca97353a!2sPoliteknik%20Negeri%20Lampung!5e0!3m2!1sid!2sid!4v1688993984646!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             <div>
@@ -15,7 +23,10 @@
                     Telepon: 021-2345675, HP:
                     Email: <a href=”mailto:contact@bacanovel.com”>contact@bacanovel.com</a>
                 </p>
-            </div>  
+            </div> 
+            <div class="col-md-6">
+            <a href="/novel/index" class="btn btn-outline-dark">Kembali</a>
+            </div> 
         </div>
     </div>
 </div>
